a2390b4cf8a4918794cbfdda795085229ade800c
sergio NUNEZ <sergio.nunez@laposte.net>
2023-07-13 19:45:47 +0200

    COMPONETS : footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatFormFieldModule } from  '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormErrorComponent } from './components/form-error/form-error.component';
 import { MatInputModule } from '@angular/material/input';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { MatInputModule } from '@angular/material/input';
     MessageDialogComponent,
     FilterProductPipe,
     AuthComponent,
-    FormErrorComponent
+    FormErrorComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

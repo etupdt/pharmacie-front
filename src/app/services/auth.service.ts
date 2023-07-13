b7e9041b7cacbb021e7d63f3343592db53075da2
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-07-13 19:43:33 +0200

    correction of router link

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Auth } from '../classes/auth';
+import { Router } from '@angular/router';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,28 @@ export class AuthService {
 
   auth!: Auth
 
-  constructor() { }
+  selectedTab: string = 'Accueil'
+  lastSelectedTab: string = 'Accueil'
+  selectedTabIndex: number = 0
+  selectedTabIndexObs: Observable<number> = new Observable<number>()
+
+  constructor(
+    private router: Router,
+  ) { }
 
   authInit = new Auth(0, '', '', '', '', '', '', '')
+
+  callRoute = (target?: number) => {
+
+    if (!isNaN(Number(target))) {
+      this.selectedTab = this.router.config[target!].path!
+    }
+
+    localStorage.setItem('selectedTab', `${this.selectedTab}`)
+    localStorage.setItem('selectedTabTime', `${(new Date()).getTime()}`)
+
+    this.router.navigate([this.selectedTab])
+
+  }
 
 }

8bfe0d6ecaa9df4e77fca8f5535c56986c010065
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-07-19 14:00:51 +0200

    change parmacie to pharmacie

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'parmacie-front'`, () => {
+  it(`should have as title 'pharmacie-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('parmacie-front');
+    expect(app.title).toEqual('pharmacie-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('parmacie-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pharmacie-front app is running!');
   });
 });

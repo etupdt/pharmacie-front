5d5f9c1976d345f3329863ff150524b7dfdf2da0
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-07-20 12:29:23 +0200

    add flags to langage selection

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -19,11 +20,15 @@ export class FooterComponent implements OnInit {
   constructor (
     private router: Router,
     private productService: ProductService,
-    private authService: AuthService
-  ) {}
+    private authService: AuthService,
+    private translate: TranslateService
+  ) {
+    translate.setDefaultLang('fr');
+  }
 
   ngOnInit(): void {
     this.onglets = this.router.config.filter(onglet => onglet.path !== '**')
+    this.selectedLangage = 'fr'
   }
 
   callRoute = (target: number) =>  {
@@ -32,8 +37,9 @@ export class FooterComponent implements OnInit {
     this.authService.callRoute(target)
   }
 
-  setSelectedLangage = () => {
-    this.authService.selectedLangage.next(this.selectedLangage)
+  setSelectedLangage = (langage: string) => {
+    this.selectedLangage = langage
+    this.authService.selectedLangage.next(langage)
   }
 
 }
